af5c60ff44364e8e05d11a8310927038eac3b1bd
alisengul53 <64622169+alisengul53@users.noreply.github.com>
2021-09-16 11:45:59 +0300

    Small fix for use_public_ip

--- KubernetesExternalClients/python/client.py
+++ KubernetesExternalClients/python/client.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
 
     client = hazelcast.HazelcastClient(
         cluster_members=["<EXTERNAL-IP>"],
-        use_public_addresses=True
+        use_public_ip=True
     )
 
     my_map = client.get_map("map_for_python").blocking()
