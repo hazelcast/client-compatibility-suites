58d748f99bc6f46615501c8eb26514c576e42d30
alisengul53 <64622169+alisengul53@users.noreply.github.com>
2022-02-20 14:37:29 +0300

    Update rc.sh

--- rc.sh
+++ rc.sh
@@ -49,24 +49,13 @@ download () {
   local repo=$1
   local jar_path=$2
   local artifact=$3
-  # Set this to anything in the caller to ignore the download error
-  local ignore_err=${4:-}
-  local err
-  local output
   if [ -f "$jar_path" ]; then
       log_info "$jar_path already exists, skipping download."
   else
       log_info "Downloading: $jar_path ($artifact) from: $repo"
-      set +e
-      output=$(mvn -q dependency:get -DrepoUrl=$repo -Dartifact=$artifact -Dtransitive=false -Ddest="$jar_path" 2>&1)
-      err=$?
-      set -e
-      if [ $err -ne 0 ]; then
-        if [ "x$ignore_err" == "x" ]; then
-            log_fatal "Failed downloading $jar_path ($artifact) from: $repo\nOutput:\n$output"
-        else
-          log_info "Ignoring the download error for $jar_path ($artifact) from: $repo"
-        fi
+      mvn -q dependency:get -DrepoUrl=$repo -Dartifact=$artifact -Ddest="$jar_path"
+      if [ $? -ne 0 ]; then
+          log_fatal "Failed downloading $jar_path ($artifact) from: $repo"
       fi
   fi
 }
@@ -89,13 +78,6 @@ downloadHazelcast () {
   local jar_path="hazelcast-${HZ_VERSION}.jar"
   local artifact="com.hazelcast:hazelcast:${HZ_VERSION}"
   download "${repo}" "$jar_path" "$artifact"
-  classpath="$classpath:$jar_path"
-}
-
-downloadSQL () {
-  local jar_path="hazelcast-sql-${HZ_VERSION}.jar"
-  local artifact="com.hazelcast:hazelcast-sql:${HZ_VERSION}"
-  download "${repo}" "$jar_path" "$artifact" ignore
   classpath="$classpath:$jar_path"
 }
 
@@ -135,8 +117,6 @@ startRC () {
   # Download Hazelcast Community jars
   downloadTests
   downloadHazelcast
-  # Download the SQL jar
-  downloadSQL
 
   if [ "x${HAZELCAST_ENTERPRISE_KEY:-}" != "x" ]; then
       # Download Hazelcast Enterprise jars
@@ -170,7 +150,7 @@ help () {
 
 TIMESTAMP_FMT="+%Y-%m-%d %H:%M:%S"
 PID_FILE="test.pid"
-HZ_VERSION="${HZ_VERSION:-5.1-SNAPSHOT}"
+HZ_VERSION="${HZ_VERSION:-4.2}"
 HAZELCAST_TEST_VERSION=${HZ_VERSION}
 HAZELCAST_ENTERPRISE_VERSION=${HZ_VERSION}
 HAZELCAST_RC_VERSION="0.8-SNAPSHOT"
