6a4c5b67e8762ae031db439cc0cff68d1b7b51a3
Yunus Inci <yemreinci0@gmail.com>
2022-03-07 18:20:46 +0300

    Add parametrized tests and more

--- HazelcastCloudTests/CppHazelcastCloudTests/some_test.cpp
+++ HazelcastCloudTests/CppHazelcastCloudTests/some_test.cpp
@@ -6,22 +6,99 @@
 
 namespace {
 
+class SomeFixture : public ::testing::Test
+{
+public:
+    static void SetUpTestSuite() {}
+
+    SomeFixture() {}
+
+    void SetUp() {}
+    void TearDown() final {}
+
+    static void TearDownTestSuite() {}
+};
+
+TEST_F(SomeFixture, test1) {}
+
+TEST_F(SomeFixture, test2) {}
+
+class SomeParametrizedTest : public ::testing::TestWithParam<std::vector<bool>>
+{
+public:
+    static void SetUpTestSuite() { rc.getHazelcastCloudCluster(cc, "1552"); }
+
+    hazelcast::client::client_config create_client_config()
+    {
+        const auto is_smart = GetParam()[0];
+        const auto ssl_enabled = GetParam()[1];
+
+        hazelcast::client::client_config cfg;
+        cfg.get_connection_strategy_config()
+          .get_retry_config()
+          .set_cluster_connect_timeout(std::chrono::seconds(2));
+        cfg.get_network_config().set_smart_routing(is_smart);
+        cfg.set_cluster_name(cc.nameForConnect);
+        cfg.get_network_config().get_cloud_config().enabled = true;
+        cfg.get_network_config().get_cloud_config().discovery_token = cc.token;
+        cfg.set_property("hazelcast.client.cloud.url", "uat.hazelcast.cloud");
+        cfg.get_network_config().get_ssl_config().set_enabled(ssl_enabled);
+        return std::move(cfg);
+    }
+
+    SomeParametrizedTest()
+      : cli{ hazelcast::new_client(create_client_config()).get() }
+    {}
+
+    void SetUp() {}
+    void TearDown() final {}
+
+    static void TearDownTestSuite()
+    {
+        // rc.deleteHazelcastCloudCluster(cc.id);
+    }
+
+    static hazelcast::util::rc::RemoteControllerClient rc;
+    static hazelcast::util::rc::CloudCluster cc;
+    hazelcast::client::hazelcast_client cli;
+};
+
+hazelcast::util::rc::RemoteControllerClient SomeParametrizedTest::rc{
+    hazelcast::util::make_remote_controller_client()
+};
+hazelcast::util::rc::CloudCluster SomeParametrizedTest::cc;
+
+TEST_P(SomeParametrizedTest, test1)
+{
+    auto map = cli.get_map("some-map").get();
+
+    map->put<std::string, std::string>("foo", "bar").get();
+
+    EXPECT_EQ(std::string("bar"),
+              (map->get<std::string, std::string>("foo").get()));
+}
+
+INSTANTIATE_TEST_SUITE_P(SomeParametrizedTest,
+                         SomeParametrizedTest,
+                         ::testing::Values(std::vector<bool>{ false, false },
+                                           std::vector<bool>{ true, false },
+                                           std::vector<bool>{ false, true },
+                                           std::vector<bool>{ true, true }));
 
 TEST(SomeTest, SomeCase)
 {
     auto rc = hazelcast::util::make_remote_controller_client();
 
     hazelcast::util::rc::CloudCluster cc;
-    rc.createHazelcastCloudStandardCluster(cc, "5.0.2-2", false);
-
+    // rc.createHazelcastCloudStandardCluster(cc, "5.0.2-2", false);
+    rc.getHazelcastCloudCluster(cc, "1552");
     //    std::cout << "name: " << cc.nameForConnect << std::endl;
     //    std::cout << "token: " << cc.token << std::endl;
 
     hazelcast::client::client_config cfg;
-    cfg.set_cluster_name("ua-1551");
+    cfg.set_cluster_name(cc.nameForConnect);
     cfg.get_network_config().get_cloud_config().enabled = true;
-    cfg.get_network_config().get_cloud_config().discovery_token =
-      "bFRPjepP1LWXvoQ62mS6gu6Qwm28uG4wMd6Ra1SCwHQrBE3RKw";
+    cfg.get_network_config().get_cloud_config().discovery_token = cc.token;
     cfg.set_property("hazelcast.client.cloud.url", "uat.hazelcast.cloud");
 
     auto cli = hazelcast::new_client(std::move(cfg)).get();
@@ -33,7 +110,5 @@ TEST(SomeTest, SomeCase)
     EXPECT_EQ(std::string("bar"),
               (map->get<std::string, std::string>("foo").get()));
 }
-
-
 
 } // namespace
