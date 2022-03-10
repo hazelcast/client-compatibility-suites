6f4a3dc2f922fe6313fcf91436c8799a9ddbdf25
alisengul53 <ali.sengul@hazelcast.com>
2022-03-10 12:53:29 +0300

    Logger added for python

--- HazelcastCloudTests/PythonHazelcastCloudTests/standard_cluster_test.py
+++ HazelcastCloudTests/PythonHazelcastCloudTests/standard_cluster_test.py
@@ -23,6 +23,7 @@ class StandardClusterTests(unittest.TestCase):
     ssl_disabled_cluster: CloudCluster = None
     rc: HzRemoteController = None
     HazelcastCloudDiscovery._CLOUD_URL_BASE = os.getenv('baseUrl').replace("https://", "")
+    logger = logging.getLogger("test")
 
     @classmethod
     def setUpClass(cls) -> None:
@@ -33,33 +34,37 @@ class StandardClusterTests(unittest.TestCase):
     @parameterized.expand([(True, True), (False, True), (True, False), (False, False)])
     def test_ssl_cluster(self, is_smart, is_ssl_enabled):
         if is_ssl_enabled:
-            hazelcast.client._logger.info("Create ssl enabled client config for smart routing " + str(is_smart))
+            self.logger.info("Create ssl enabled client config for smart routing " + str(is_smart))
             client = hazelcast.HazelcastClient(
-                **HelperMethods.create_client_config_with_ssl(self.ssl_enabled_cluster.nameForConnect, self.ssl_enabled_cluster.token, is_smart,
-                                                              self.ssl_enabled_cluster.certificatePath, self.ssl_enabled_cluster.tlsPassword))
+                **HelperMethods.create_client_config_with_ssl(self.ssl_enabled_cluster.nameForConnect,
+                                                              self.ssl_enabled_cluster.token, is_smart,
+                                                              self.ssl_enabled_cluster.certificatePath,
+                                                              self.ssl_enabled_cluster.tlsPassword))
             cluster = self.ssl_enabled_cluster
         else:
-            hazelcast.client._logger.info("Create ssl disabled client config for smart routing " + str(is_smart))
-            client = hazelcast.HazelcastClient(**HelperMethods.create_client_config(self.ssl_disabled_cluster.nameForConnect, self.ssl_disabled_cluster.token, is_smart))
+            self.logger.info("Create ssl disabled client config for smart routing " + str(is_smart))
+            client = hazelcast.HazelcastClient(
+                **HelperMethods.create_client_config(self.ssl_disabled_cluster.nameForConnect,
+                                                     self.ssl_disabled_cluster.token, is_smart))
             cluster = self.ssl_disabled_cluster
 
         HelperMethods.map_put_get_and_verify(self, client.get_map("map_for_ssl_cluster").blocking())
 
-        hazelcast.client._logger.info("Scaling up cluster from 2 node to 4")
+        self.logger.info("Scaling up cluster from 2 node to 4")
         self.rc.scaleUpDownStandardCluster(cluster.id, 2)
         HelperMethods.map_put_get_and_verify(self, client.get_map("map_for_ssl_cluster").blocking())
 
-        hazelcast.client._logger("Scaling down cluster from 4 node to 2")
+        self.logger.info("Scaling down cluster from 4 node to 2")
         self.rc.scaleUpDownStandardCluster(cluster.id, -2)
         HelperMethods.map_put_get_and_verify(self, client.get_map("map_for_ssl_cluster").blocking())
 
-        hazelcast.client._logger("Stopping cluster")
+        self.logger.info("Stopping cluster")
         self.rc.stopCluster(cluster.id)
 
-        hazelcast.client._logger("Resuming cluster")
+        self.logger.info("Resuming cluster")
         self.rc.resumeCluster(cluster.id)
 
-        hazelcast.client._logger("Wait 5 seconds to be sure client is connected")
+        self.logger.info("Wait 5 seconds to be sure client is connected")
         time.sleep(5)
         HelperMethods.map_put_get_and_verify(self, client.get_map("map_for_ssl_cluster").blocking())
 
@@ -68,7 +73,8 @@ class StandardClusterTests(unittest.TestCase):
     @parameterized.expand([(True,), (False,)])
     def test_try_connect_ssl_cluster_without_certificates(self, is_smart):
         with self.assertRaises(IllegalStateError):
-            config = HelperMethods.create_client_config(self.ssl_enabled_cluster.nameForConnect, self.ssl_enabled_cluster.token, is_smart)
+            config = HelperMethods.create_client_config(self.ssl_enabled_cluster.nameForConnect,
+                                                        self.ssl_enabled_cluster.token, is_smart)
             config["ssl_enabled"] = True
             config["cluster_connect_timeout"] = 10
             hazelcast.HazelcastClient(**config)
