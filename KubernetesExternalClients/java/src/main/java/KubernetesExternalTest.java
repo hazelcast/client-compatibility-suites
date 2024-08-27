9e3068dfa4a94d50842b6ebf518d3d43e9892ca7
Serkan Özel <serkan.ozel@hazelcast.com>
2024-08-27 19:14:38 +0300

    Add try catch

--- KubernetesExternalClients/java/src/main/java/KubernetesExternalTest.java
+++ KubernetesExternalClients/java/src/main/java/KubernetesExternalTest.java
@@ -14,7 +14,7 @@ public class KubernetesExternalTest {
 
     private static final Logger LOGGER = Logger.getLogger(KubernetesExternalTest.class.getName());
 
-    public static void main( String[] args ) throws InterruptedException {
+    public static void main( String[] args ) {
         String externalIp = "<EXTERNAL-IP>";
 
         ClientConfig clientConfig = new ClientConfig();
@@ -23,20 +23,25 @@ public class KubernetesExternalTest {
         HazelcastInstance client = HazelcastClient.newHazelcastClient(clientConfig);
         LOGGER.info("Successful connection!");
 
-        IMap<String, String> map =  client.getMap("mapForJava");
-        LOGGER.info("Starting to fill the map with random entries.");
+        try {
+            IMap<String, String> map =  client.getMap("mapForJava");
+            LOGGER.info("Starting to fill the map with random entries.");
 
-        Random random = ThreadLocalRandom.current();
-        for (int i = 0; i < 120; i++) {
-            int randomKey = random.nextInt(100000);
-            try {
-                map.put("key" + randomKey, "value" + randomKey);
-            } catch (Throwable exc) {
-                LOGGER.warning("Put operation failed: " + exc);
+            Random random = ThreadLocalRandom.current();
+            for (int i = 0; i < 120; i++) {
+                int randomKey = random.nextInt(100000);
+                try {
+                    map.put("key" + randomKey, "value" + randomKey);
+                } catch (Throwable exc) {
+                    LOGGER.warning("Put operation failed: " + exc);
+                }
+                int size = map.size();
+                LOGGER.info("Current map size: " + size);
+                Thread.sleep(1000);
             }
-            int size = map.size();
-            LOGGER.info("Current map size: " + size);
-            Thread.sleep(1000);
+        } catch (Throwable e) {
+            LOGGER.severe("Shutting down the client, an error occurred: " + e);
+            client.shutdown();
         }
     }
 }
--- KubernetesInternalClients/java/src/main/java/KubernetesInternalTest.java
+++ KubernetesInternalClients/java/src/main/java/KubernetesInternalTest.java
@@ -14,35 +14,42 @@ public class KubernetesInternalTest {
 
     private static final Logger LOGGER = Logger.getLogger(KubernetesInternalTest.class.getName());
 
-    public static void main( String[] args ) throws InterruptedException {
+    public static void main( String[] args ) {
         String internalIp = "hz-hazelcast";
 
         ClientConfig clientConfig = new ClientConfig();
         clientConfig.getNetworkConfig().addAddress(internalIp);
 
         HazelcastInstance client = HazelcastClient.newHazelcastClient(clientConfig);
-        IMap<String, String> map =  client.getMap("map");
-        map.put("key", "value");
-        String res = map.get("key");
-        if (!res.equals("value")) {
-            LOGGER.warning("Connection failed, check your configuration.");
-        }
-        LOGGER.info("Successful connection!");
-        LOGGER.info("Starting to fill the map with random entries.");
 
-        Random random = ThreadLocalRandom.current();
-        while (true) {
-            int randomKey = random.nextInt(100000);
-            try {
-                map.put("key" + randomKey, "value" + randomKey);
-            } catch (Throwable exc) {
-                LOGGER.warning("Put operation failed: " + exc);
+        try {
+            IMap<String, String> map =  client.getMap("map");
+            map.put("key", "value");
+            String res = map.get("key");
+            if (!res.equals("value")) {
+                LOGGER.warning("Connection failed, check your configuration.");
             }
-            if (randomKey % 100 == 0) {
-                int size = map.size();
-                LOGGER.info("Current map size: " + size);
-                Thread.sleep(1000);
+            LOGGER.info("Successful connection!");
+            LOGGER.info("Starting to fill the map with random entries.");
+
+            Random random = ThreadLocalRandom.current();
+            while (true) {
+                int randomKey = random.nextInt(100000);
+                try {
+                    map.put("key" + randomKey, "value" + randomKey);
+                } catch (Throwable exc) {
+                    LOGGER.warning("Put operation failed: " + exc);
+                }
+                if (randomKey % 100 == 0) {
+                    int size = map.size();
+                    LOGGER.info("Current map size: " + size);
+                    Thread.sleep(1000);
+                }
             }
+        } catch (Throwable e) {
+            LOGGER.severe("An error occurred, shutting down the client: " + e);
+            client.shutdown();
         }
+
     }
 }
