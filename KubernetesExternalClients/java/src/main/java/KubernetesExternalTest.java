8064cfc77255a7742c30b5c19db85d5d125f5dbe
Serkan Özel <serkan.ozel@hazelcast.com>
2024-08-29 01:00:19 +0300

    Fixes

--- KubernetesExternalClients/java/src/main/java/KubernetesExternalTest.java
+++ KubernetesExternalClients/java/src/main/java/KubernetesExternalTest.java
@@ -1,7 +1,9 @@
 import com.hazelcast.client.HazelcastClient;
 import com.hazelcast.client.config.ClientConfig;
+import com.hazelcast.client.properties.ClientProperty;
 import com.hazelcast.core.HazelcastInstance;
 import com.hazelcast.map.IMap;
+import com.hazelcast.spi.properties.HazelcastProperty;
 
 import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
@@ -12,13 +14,22 @@ import java.util.logging.Logger;
  */
 public class KubernetesExternalTest {
 
-    private static final Logger LOGGER = Logger.getLogger(KubernetesExternalTest.class.getName());
+    private static final Logger LOGGER;
+    static {
+        // The following makes jdk logging appear in single lines instead of 2 lines:
+        // https://stackoverflow.com/a/10706033
+        System.setProperty("java.util.logging.SimpleFormatter.format",
+                "%1$tY-%1$tm-%1$td %1$tH:%1$tM:%1$tS %4$s %2$s %5$s%6$s%n");
+        LOGGER = Logger.getLogger(KubernetesExternalTest.class.getName());
+    }
 
     public static void main( String[] args ) {
-        String externalIp = "<EXTERNAL-IP>";
+        String externalIp = "127.0.0.1";
 
         ClientConfig clientConfig = new ClientConfig();
         clientConfig.getNetworkConfig().addAddress(externalIp);
+        // disable client logging
+        clientConfig.setProperty( "hazelcast.logging.type", "none" );
 
         HazelcastInstance client = HazelcastClient.newHazelcastClient(clientConfig);
         LOGGER.info("Successful connection!");
@@ -28,7 +39,7 @@ public class KubernetesExternalTest {
             LOGGER.info("Starting to fill the map with random entries.");
 
             Random random = ThreadLocalRandom.current();
-            for (int i = 0; i < 120; i++) {
+            for (int i = 0; i < 5; i++) {
                 int randomKey = random.nextInt(100000);
                 try {
                     map.put("key" + randomKey, "value" + randomKey);
