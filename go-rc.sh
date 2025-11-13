hazelcast/client-compatibility-suites

commit d7b0ca6a6e8f65d7b8fe1838d1c30e2d5b4ba2c7

Relocate `hazelcast-remote-controller` [DI-639] (#190)
In https://github.com/hazelcast/hazelcast-remote-controller/pull/75,
`hazelcast-remote-controller` is deployed to a different repository to
address build issues. Update the paths to suit.

Also:
- updated Maven central URL as per
https://github.com/hazelcast/hazelcast-mono/pull/4897
- updated Maven central URL to use `HTTPS`, as `HTTP` is [no longer
supported](https://maven.apache.org/docs/3.8.1/release-notes.html).

Fixes: [DI-639](https://hazelcast.atlassian.net/browse/DI-639)

[DI-639]:
https://hazelcast.atlassian.net/browse/DI-639?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ

go-rc.sh
@@ -74,7 +74,7 @@ download () {
 downloadRC () {
   local jar_path="hazelcast-remote-controller-${HAZELCAST_RC_VERSION}.jar"
   local artifact="com.hazelcast:hazelcast-remote-controller:${HAZELCAST_RC_VERSION}"
-  download "$SNAPSHOT_REPO" "$jar_path" "$artifact"
+  download "$ENTERPRISE_RELEASE_REPO" "$jar_path" "$artifact"
   classpath="$classpath:$jar_path"
 }
 
@@ -175,7 +175,7 @@ HAZELCAST_TEST_VERSION=${HZ_VERSION}
 HAZELCAST_ENTERPRISE_VERSION=${HZ_VERSION}
 HAZELCAST_RC_VERSION="0.8-SNAPSHOT"
 SNAPSHOT_REPO="https://oss.sonatype.org/content/repositories/snapshots"
-RELEASE_REPO="http://repo1.maven.apache.org/maven2"
+RELEASE_REPO="https://repo.maven.apache.org/maven2"
 ENTERPRISE_RELEASE_REPO="https://repository.hazelcast.com/release/"
 ENTERPRISE_SNAPSHOT_REPO="https://repository.hazelcast.com/snapshot/"
 

HazelcastCloudTests/javahazelcastcloudtests/pom.xml
@@ -82,14 +82,6 @@
     </dependencies>
     <repositories>
         <repository>
-            <id>oss-sonatype</id>
-            <name>oss-sonatype</name>
-            <url>https://oss.sonatype.org/content/repositories/snapshots/</url>
-            <snapshots>
-                <enabled>true</enabled>
-            </snapshots>
-        </repository>
-        <repository>
             <id>private-repository</id>
             <name>Hazelcast Private Repository</name>
             <url>https://repository.hazelcast.com/snapshot/</url>

start_rc.py
@@ -7,8 +7,7 @@ from contextlib import closing
 from os import path
 
 from util import (
-    SNAPSHOT_REPO,
-    RELEASE_REPO,
+    ENTERPRISE_SNAPSHOT_REPO,
     download_via_maven,
     IS_ON_WINDOWS,
     ServerKind,
@@ -61,12 +60,7 @@ def parse_args() -> argparse.Namespace:
 def start_rc(
     rc_version: str, dst_folder: str, use_simple_server: bool, server_kind: ServerKind
 ) -> None:
-    if rc_version.upper().endswith("-SNAPSHOT"):
-        rc_repo = SNAPSHOT_REPO
-    else:
-        rc_repo = RELEASE_REPO
-
-    download_via_maven(rc_repo, "hazelcast-remote-controller", rc_version, dst_folder)
+    download_via_maven(ENTERPRISE_SNAPSHOT_REPO, "hazelcast-remote-controller", rc_version, dst_folder)
     class_path = path.join(dst_folder, "*")
 
     args = [

start_remote_controller.py
@@ -7,7 +7,7 @@ from os.path import isfile
 SERVER_VERSION = "5.0"
 RC_VERSION = "0.8-SNAPSHOT"
 
-RELEASE_REPO = "https://repo1.maven.apache.org/maven2"
+RELEASE_REPO = "https://repo.maven.apache.org/maven2"
 ENTERPRISE_RELEASE_REPO = "https://repository.hazelcast.com/release/"
 SNAPSHOT_REPO = "https://oss.sonatype.org/content/repositories/snapshots"
 ENTERPRISE_SNAPSHOT_REPO = "https://repository.hazelcast.com/snapshot/"
@@ -80,7 +80,7 @@ def download_if_necessary(repo, artifact_id, version, is_test_artifact=False):
 def start_rc(use_simple_server: bool, stdout=None, stderr=None):
     artifacts = []
 
-    rc = download_if_necessary(RC_REPO, "hazelcast-remote-controller", RC_VERSION)
+    rc = download_if_necessary(ENTERPRISE_SNAPSHOT_REPO, "hazelcast-remote-controller", RC_VERSION)
     tests = download_if_necessary(REPO, "hazelcast", SERVER_VERSION, True)
     sql = download_if_necessary(REPO, "hazelcast-sql", SERVER_VERSION)
 

util.py
@@ -49,7 +49,7 @@ HAZELCAST_SERVERS = "https://raw.githubusercontent.com/hazelcast/rel-scripts/mas
 
 CLIENT_HEADER = "======= %s Client\n---\n(.*?)\n---\n==="
 
-RELEASE_REPO = "http://repo1.maven.apache.org/maven2"
+RELEASE_REPO = "https://repo.maven.apache.org/maven2"
 ENTERPRISE_RELEASE_REPO = "https://repository.hazelcast.com/release/"
 SNAPSHOT_REPO = "https://oss.sonatype.org/content/repositories/snapshots"
 ENTERPRISE_SNAPSHOT_REPO = "https://repository.hazelcast.com/snapshot/"

commit c6a4d3024992f62b82b406a07d1b603f1b4d5679

Fix Maven RCD Download Maven Repo (#195)
It was
[reported](https://hazelcast.slack.com/archives/C08S5ARTJF8/p1762868364580739)
that the Go compatibility test [is
failing](https://github.com/hazelcast/client-compatibility-suites/actions/runs/19267010409/job/55086083566#step:10:46)
when downloading RCD:
```
2025-11-11 13:33:27 INFO : Downloading: hazelcast-remote-controller-0.8-SNAPSHOT.jar (com.hazelcast:hazelcast-remote-controller:0.8-SNAPSHOT) from: https://repository.hazelcast.com/release/
2025-11-11 13:33:41 FATAL: Failed downloading hazelcast-remote-controller-0.8-SNAPSHOT.jar (com.hazelcast:hazelcast-remote-controller:0.8-SNAPSHOT) from: https://repository.hazelcast.com/release/
```

It should download from the `snapshot`, not `release` repo.

This was broken in
https://github.com/hazelcast/client-compatibility-suites/pull/190, but
we can simplify the script to reduce this kind of problem in future:
- in https://github.com/hazelcast/client-compatibility-suites/pull/188,
all downloads were updated to use EE repos - as such, removed non-EE
repo definition (and the corresponding selection logic)
- moved the _existing_ check to figure out whether to use `snapshot` or
`release` repo from being set up-front based on the _Hazelcast_ version,
to being done dynamically in `download` based on the artifact being
downloaded
- this avoids the need to specify the repo _at all_ for remote
controller

[Example
execution](https://github.com/hazelcast/client-compatibility-suites/actions/runs/19271064457/job/55099195599).

---------

Co-authored-by: Yüce Tekol <yucetekol@gmail.com>

go-rc.sh
@@ -46,16 +46,21 @@ log_fatal () {
 }
 
 download () {
-  local repo=$1
-  local jar_path=$2
-  local artifact=$3
+  local jar_path=$1
+  local artifact=$2
   # Set this to anything in the caller to ignore the download error
-  local ignore_err=${4:-}
+  local ignore_err=${3:-}
   local err
   local output
   if [ -f "$jar_path" ]; then
       log_info "$jar_path already exists, skipping download."
   else
+      if [[ "${artifact}" == *-SNAPSHOT ]]; then
+          repo=${ENTERPRISE_SNAPSHOT_REPO}
+      else
+          repo=${ENTERPRISE_RELEASE_REPO}
+      fi
+      
       log_info "Downloading: $jar_path ($artifact) from: $repo"
       set +e
       output=$(mvn -q org.apache.maven.plugins:maven-dependency-plugin:2.10:get -DremoteRepositories=$repo -Dartifact=$artifact -Dtransitive=false -Ddest="$jar_path" 2>&1)
@@ -74,57 +79,48 @@ download () {
 downloadRC () {
   local jar_path="hazelcast-remote-controller-${HAZELCAST_RC_VERSION}.jar"
   local artifact="com.hazelcast:hazelcast-remote-controller:${HAZELCAST_RC_VERSION}"
-  download "$ENTERPRISE_RELEASE_REPO" "$jar_path" "$artifact"
+  download "$jar_path" "$artifact"
   classpath="$classpath:$jar_path"
 }
 
 downloadTests () {
   local jar_path="hazelcast-${HAZELCAST_TEST_VERSION}-tests.jar"
   local artifact="com.hazelcast:hazelcast:${HAZELCAST_TEST_VERSION}:jar:tests"
-  download "${enterprise_repo}" "$jar_path" "$artifact"
+  download "$jar_path" "$artifact"
   classpath="$classpath:$jar_path"
 }
 
 downloadHazelcast () {
   local jar_path="hazelcast-${HZ_VERSION}.jar"
   local artifact="com.hazelcast:hazelcast:${HZ_VERSION}"
-  download "${enterprise_repo}" "$jar_path" "$artifact"
+  download "$jar_path" "$artifact"
   classpath="$classpath:$jar_path"
 }
 
 downloadSQL () {
   local jar_path="hazelcast-sql-${HZ_VERSION}.jar"
   local artifact="com.hazelcast:hazelcast-sql:${HZ_VERSION}"
-  download "${enterprise_repo}" "$jar_path" "$artifact"
+  download "$jar_path" "$artifact"
   classpath="$classpath:$jar_path"
 }
 
 downloadHazelcastEnterprise () {
   local jar_path="hazelcast-enterprise-${HAZELCAST_ENTERPRISE_VERSION}.jar"
   local artifact="com.hazelcast:hazelcast-enterprise:${HAZELCAST_ENTERPRISE_VERSION}"
-  download "${enterprise_repo}" "$jar_path" "$artifact"
+  download "$jar_path" "$artifact"
   classpath="$classpath:$jar_path"
 }
 
 downloadTestsEnterprise () {
   local jar_path="hazelcast-enterprise-${HAZELCAST_ENTERPRISE_VERSION}-tests.jar"
   local artifact="com.hazelcast:hazelcast-enterprise:${HAZELCAST_ENTERPRISE_VERSION}:jar:tests"
-  download "${enterprise_repo}" "$jar_path" "$artifact" ignore
+  download "$jar_path" "$artifact" ignore
   classpath="$classpath:$jar_path"
 }
 
 startRC () {
   if [ -f "$PID_FILE" ]; then
     log_fatal "PID file $PID_FILE exists. Is there an another instance of Hazelcast Remote Controller running?"
-  fi
-
-  if [[ "${HZ_VERSION}" == *-SNAPSHOT ]]
-  then
-    repo=${SNAPSHOT_REPO}
-    enterprise_repo=${ENTERPRISE_SNAPSHOT_REPO}
-  else
-    repo=${RELEASE_REPO}
-    enterprise_repo=${ENTERPRISE_RELEASE_REPO}
   fi
 
   classpath=""
@@ -174,8 +170,6 @@ HZ_VERSION="${HZ_VERSION:-5.3.1}"
 HAZELCAST_TEST_VERSION=${HZ_VERSION}
 HAZELCAST_ENTERPRISE_VERSION=${HZ_VERSION}
 HAZELCAST_RC_VERSION="0.8-SNAPSHOT"
-SNAPSHOT_REPO="https://oss.sonatype.org/content/repositories/snapshots"
-RELEASE_REPO="https://repo.maven.apache.org/maven2"
 ENTERPRISE_RELEASE_REPO="https://repository.hazelcast.com/release/"
 ENTERPRISE_SNAPSHOT_REPO="https://repository.hazelcast.com/snapshot/"
 