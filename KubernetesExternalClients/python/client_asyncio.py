4ef773727df0032fd1e3e68d365a4e071403dfa2
Yuce Tekol <yucetekol@gmail.com>
2026-01-16 01:42:39 +0300

    Added updated asyncio client

--- KubernetesExternalClients/python/client_asyncio.py
+++ KubernetesExternalClients/python/client_asyncio.py
@@ -1,32 +1,39 @@
-import hazelcast
+
+import asyncio
 import logging
 import random
 
-if __name__ == "__main__":
+from hazelcast.asyncio import HazelcastClient
+
+async def amain():
     logging.basicConfig(level=logging.INFO)
 
-    client = hazelcast.HazelcastClient(
+    client = await HazelcastClient.create_and_start(
         cluster_members=["<EXTERNAL-IP>"],
         use_public_ip=True,
     )
 
-    my_map = client.get_map("map_for_python").blocking()
-    my_map.put("key", "value")
+    my_map = await client.get_map("map_for_python")
+    await my_map.put("key", "value")
 
-    if my_map.get("key") == "value":
+    if await my_map.get("key") == "value":
         print("Successful connection!")
         print("Starting to fill the map with random entries.")
 
         for _ in range(120):
             random_key = random.randint(1, 100000)
             try:
-                my_map.put(f"key-{random_key}", f"value-{random_key}")
-            except:
+                await my_map.put(f"key-{random_key}", f"value-{random_key}")
+            except Exception:
                 logging.exception("Put operation failed!")
 
-            map_size = my_map.size()
+            map_size = await my_map.size()
             print(f"Current map size: {map_size}")
     else:
         raise Exception("Connection failed, check your configuration.")
 
-    client.shutdown()
+    await client.shutdown()
+
+
+if __name__ == "__main__":
+    asyncio.run(amain())
