fbd6920e8df8b56bc516b25bc3509e8504387b51
Yüce Tekol <yucetekol@gmail.com>
2026-02-04 09:14:11 +0300

    Added Kubernetes tests for Python asyncio client (#199)
    Not sure if it works yet, but won't be able to test it without getting
    it merged.
    
    ---------
    
    Co-authored-by: Jack Green <jack.green@hazelcast.com>

---
+++ KubernetesExternalClients/python/client_asyncio.py
@@ -0,0 +1,40 @@
+
+import asyncio
+import logging
+import random
+
+from hazelcast.asyncio import HazelcastClient
+
+async def amain():
+    logging.basicConfig(level=logging.INFO)
+
+    client = await HazelcastClient.create_and_start(
+        cluster_members=["<EXTERNAL-IP>"],
+        use_public_ip=True,
+    )
+
+    my_map = await client.get_map("map_for_python")
+    await my_map.put("key", "value")
+
+    if await my_map.get("key") == "value":
+        print("Successful connection!")
+        print("Starting to fill the map with random entries.")
+
+        for _ in range(120):
+            random_key = random.randint(1, 100000)
+            try:
+                await my_map.put(f"key-{random_key}", f"value-{random_key}")
+            except Exception:
+                logging.exception("Put operation failed!")
+
+            map_size = await my_map.size()
+            print(f"Current map size: {map_size}")
+    else:
+        raise Exception("Connection failed, check your configuration.")
+
+    await client.shutdown()
+
+
+if __name__ == "__main__":
+    asyncio.run(amain())
+  
